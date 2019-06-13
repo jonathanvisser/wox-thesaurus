6abaff2d5c93c48df7694823aa86c4ebafce47f3
Jonathan Visser <jonathan.visser@certussolutions.com>
2019-06-13 22:57:39 +1200

    got multi-line formatting working

--- main.py
+++ main.py
@@ -17,6 +17,42 @@ class Thesaurus(Wox):
     r = requests.get(url)
     j = json.loads(r.text)
     return j
+  def sizeFragments(self, results, wordtype,aWords,key,imagePath):
+    # loop through array
+    iRunningTotal = 0
+    h = 0
+    firstTimeFlag = True
+    for i in range(0,len(aWords)):
+
+      # count letters in word
+      iRunningTotal = iRunningTotal + len(aWords[i])
+      # if running total > 80 then create fragment array
+      if (iRunningTotal > 70) or (i == len(aWords)-1):
+        #print("h: " + str(h) + "   i: " + str(i))
+        if firstTimeFlag == True:
+          #print("FIRST TIME:")
+          #print(aWords[h:i+1])
+          self.addResult(results, wordtype,aWords[h:i+1],key,imagePath)
+          firstTimeFlag = False
+        else:
+          imagePath = "Images/blank.ico"
+          key =""
+          wordtype = ""
+          self.addResult(results, wordtype,aWords[h:i+1],key,imagePath)
+          #print(aWords[h:i+1])
+        h = i + 1
+        iRunningTotal = 0
+    
+
+  def addResult(self, results, wordtype,words,key,imagePath):
+    if key != "":
+      key = RELATIONSHIP_ABBR[key]
+    words = ', '.join(words)
+    results.append({
+      "Title": "{}{}".format(wordtype,words),
+      "SubTitle": "{}".format(key),
+      "IcoPath": imagePath
+    })
 
   # A function named query is necessary, we will automatically invoke this function when user query this plugin
   def query(self,key):
@@ -26,30 +62,20 @@ class Thesaurus(Wox):
       # take the result set and break it up into result sets to append
       for key, value in rex.items(): # loop through word types
         previouswordtype = ""
-        wordtype = key + ": "
+        wordtype = key.upper() + ": "
         imagePath = "Images/syn.ico"
         for key, value in value.items(): # for each word type there maybe syn, ant, rel, sim, and user defined words
-          words = ', '.join(value)
+          words = value #', '.join(value)
           if previouswordtype == wordtype:
             wordtype = ""
             imagePath = "Images/" + key + ".ico"
-          wordslen = len(words)
-     #     if wordslen > 90:
-     #       linefragements =[]
-     #       fragmentsneeded = int(wordslen/90)
-     #       for i in range(0, fragmentsneeded):
-     #         linefragements.append(words[0:90])
-     #       for chunk in linefragements:
-     #         results.append({
-     #         "Title": "{}/{}:{}".format(wordslen,fragmentsneeded,chunk),
-     #         "SubTitle": "In loop",
-     #         "IcoPath": "Images/blank.ico"
-     #     })
-          results.append({
-            "Title": "{}{}".format(wordtype,words),
-            "SubTitle": "{}".format(RELATIONSHIP_ABBR[key]),
-            "IcoPath": imagePath
-          })
+          self.sizeFragments(results, wordtype,words,key,imagePath)
+
+          #results.append({
+          #  "Title": "{}{}".format(wordtype,words),
+          #  "SubTitle": "{}".format(RELATIONSHIP_ABBR[key]),
+          #  "IcoPath": imagePath
+          #})
           previouswordtype = wordtype
 
       return results
